4e290fa9cb43f128e62763a6e09dc35ec46cb392
karkran <nikhil.karkra@anz.com>
2019-02-07 22:11:47 +1100

    applying global styling

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Player from './Player';
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import GlobalStyle from '../styles/GlobalStyles'
 const App = () => (
   <BrowserRouter>
-  <Switch>
-    <Route exact path="/" component={Player}/>
-    <Route exact path="/:activeVideo" component={Player}/>
-  </Switch>
+    <>
+      <Switch>
+        <Route exact path="/" component={Player}/>
+        <Route exact path="/:activeVideo" component={Player}/>
+      </Switch>
+    <GlobalStyle/>
+    </>
   </BrowserRouter>
 )
 
