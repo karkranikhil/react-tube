db2aa6514200f14c5183c73d3836d74f070ff5af
karkran <nikhil.karkra@anz.com>
2019-02-07 22:04:47 +1100

    setting up routing

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import Player from './Player';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 const App = () => (
-  <Player/>
+  <BrowserRouter>
+  <Switch>
+    <Route exact path="/" component={Player}/>
+    <Route exact path="/:activeVideo" component={Player}/>
+  </Switch>
+  </BrowserRouter>
 )
 
 export default App;
